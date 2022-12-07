6088e252dfdc536705fabd2c415d4134a28d983f
Gerald12344 <harvey.randall@aol.com>
2022-12-07 03:04:01 +0000

    Geting closer

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,5 +1,10 @@
 package com.awesomeproject;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -24,6 +29,7 @@ public class MainActivity extends ReactActivity {
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
   }
+
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
--- android/app/src/main/java/com/awesomeproject/Panik.java
+++ android/app/src/main/java/com/awesomeproject/Panik.java
@@ -3,36 +3,69 @@ package com.awesomeproject;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.app.PendingIntent;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class Panik extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.panik);
-        views.setTextViewText(R.id.PANIKBUTTOn, widgetText);
-        
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Chain up to the super class so the onEnabled, etc callbacks get dispatched
+        super.onReceive(context, intent);
     }
+
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Toast.makeText( context,"Setup!", Toast.LENGTH_SHORT).show();
+        // There may be multiple widgets active, so update all of them.
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
+    private void updateAppWidget(Context context,
+                                 AppWidgetManager appWidgetManager,
+                                 int appWidgetId) {
+
+
+        RemoteViews views = new RemoteViews(context.getPackageName(),
+                R.layout.panik);
+
+
+        // Setup update button to send an update request as a pending intent.
+        Intent intentUpdate = new Intent(context, Panik.class);
+
+        // The intent action must be an app widget update.
+        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+        // Include the widget ID to be updated as an intent extra.
+        int[] idArray = new int[]{appWidgetId};
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+
+        // Wrap it all in a pending intent to send a broadcast.
+        // Use the app widget ID as the request code (third argument) so that
+        // each intent is unique.
+        PendingIntent pendingUpdate = PendingIntent.getBroadcast(context, appWidgetId, intentUpdate, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Assign the pending intent to the button onClick handler
+        views.setOnClickPendingIntent(R.id.PANIKBUTTOn, pendingUpdate);
+
+        // Instruct the widget manager to update the widget.
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
     @Override
     public void onEnabled(Context context) {
+        //Toast.makeText( context,"neweer!", Toast.LENGTH_SHORT).show();
         // Enter relevant functionality for when the first widget is created
     }
 
